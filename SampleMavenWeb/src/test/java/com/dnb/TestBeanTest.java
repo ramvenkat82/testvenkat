e0dd58683cc72e94628602e720ba5d8a9822213f
DNBINT\venkateshra <venkateshra@dnb.com>
2017-09-13 22:22:18 +0530

    Added test class in test folder

---
+++ SampleMavenWeb/src/test/java/com/dnb/TestBeanTest.java
@@ -0,0 +1,12 @@
+package com.dnb;
+
+import junit.framework.TestCase;
+
+public class TestBeanTest extends TestCase {
+
+	public void testIsValidCompanyId() {
+		assertTrue("Valid CompanyId", com.dnb.TestBean.isValidCompanyId(600000));
+		assertFalse("Valid CompanyId", com.dnb.TestBean.isValidCompanyId(700000));
+	}
+
+}
